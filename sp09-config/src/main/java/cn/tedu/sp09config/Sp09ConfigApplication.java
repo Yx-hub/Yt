0ed93142149a88a1322618a17d502fd9e32a1fbd
Yztt <958832670@qq.com>
2021-12-22 11:49:42 +0800

    y

---
+++ sp09-config/src/main/java/cn/tedu/sp09config/Sp09ConfigApplication.java
@@ -0,0 +1,17 @@
+package cn.tedu.sp09config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class Sp09ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sp09ConfigApplication.class, args);
+    }
+
+}
